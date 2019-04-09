aa899a4add780cc7526ae86a3e84b394c595bb19
Pawel Kwiatek <pawel.kwiatek@nokia.com>
2019-04-09 12:20:04 +0200

    pwd command

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -10,7 +10,7 @@ object Command {
 
   val MKDIR = "mkdir"
   val LS = "ls"
-
+  val PWD = "pwd"
 
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
@@ -27,6 +27,7 @@ object Command {
       if(tokens.length < 2) incompleteCommand(MKDIR)
       else new Mkdir(tokens(1))
     } else if(LS.equals(tokens(0))) new Ls
+      else if(PWD.equals(tokens(0))) new Pwd
     else new UnknownCommand
   }
 }
