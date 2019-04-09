4953ea1ba48106319657a7b769527fd9e95e265f
Pawel Kwiatek <pawel.kwiatek@nokia.com>
2019-04-09 09:56:24 +0200

    ls command

--- src/com/rtjvm/scala/oop/files/Directory.scala
+++ src/com/rtjvm/scala/oop/files/Directory.scala
@@ -30,6 +30,8 @@ class Directory(override val parentPath: String, override val name: String, val 
     new Directory(parentPath, name, contents.filter(e => !e.name.equals(entryName)) :+ newEntry)
 
   override def asDirectory: Directory = this
+
+  override def getType: String = "Directory"
 }
 
 object Directory {
