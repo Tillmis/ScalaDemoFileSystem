64e572f7c4293ad26ea86976c7f11895bb75ae49
Pawel Kwiatek <pawel.kwiatek@nokia.com>
2019-04-10 14:05:49 +0200

    echo command

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -14,6 +14,7 @@ object Command {
   val TOUCH = "touch"
   val CD = "cd"
   val RM = "rm"
+  val ECHO = "echo"
 
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
@@ -39,7 +40,10 @@ object Command {
       else new Cd(tokens(1))
     } else if(RM.equals(tokens(0))) {
       if (tokens.length < 2) incompleteCommand(RM)
-      else new Cd(tokens(1))
+      else new Rm(tokens(1))
+    } else if(ECHO.equals(tokens(0))) {
+      if (tokens.length < 2) incompleteCommand(ECHO)
+      else new Echo(tokens.tail)
     }
     else new UnknownCommand
   }
